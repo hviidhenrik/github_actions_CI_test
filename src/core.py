699c29294b14e1bc5b1d1dba0ccea6e5a08a552b
Henrik Hviid Hansen <hviidhenrik@outlook.com>
2022-01-15 23:04:57 +0100

    add workflow file and small unit test

---
+++ src/core.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def my_add(num1, num2):
+    return np.add(num1, num2)
+
+
+if __name__ == "__main__":
+    print("5 + 5 =", my_add(5, 5))
--- tests/tests.py
+++ tests/tests.py
@@ -2,5 +2,10 @@
 Unit tests go here
 """
 import pytest
+from src.core import my_add
 
+
+@pytest.mark.parametrize("input, expected", [[(5, 5), 10], [(8, 8), 16]])
+def test_my_add(input, expected):
+    assert my_add(*input) == expected
 
