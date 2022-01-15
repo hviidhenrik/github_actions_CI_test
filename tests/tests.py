0e9b88a99a283c14f69a90d2b710d0ba187fd8a1
Henrik Hviid Hansen <hviidhenrik@outlook.com>
2022-01-15 23:14:50 +0100

    add workflow file and small unit test

--- tests/tests.py
+++ tests/tests.py
@@ -7,5 +7,12 @@ from src.core import my_add
 
 @pytest.mark.parametrize("input, expected", [[(5, 5), 10], [(8, 8), 16]])
 def test_my_add(input, expected):
+    """
+    Tests add function
+
+    :param input:
+    :param expected:
+    :return:
+    """
     assert my_add(*input) == expected
 
